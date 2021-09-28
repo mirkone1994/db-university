bd48fa46f34886df07440552eb874f26f706bb73
mirkone1994 <mirkochiappori@gmail.com>
2021-09-28 16:49:51 +0200

    8 select

--- university.sql
+++ university.sql
@@ -26,3 +26,6 @@ SELECT * FROM `degrees`
 where `level` = 'magistrale'
 --7 Da quanti dipartimenti è composta l'università? (12)
 SELECT `name` FROM `departments`
+--8 Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * FROM `teachers`
+WHERE `phone` IS NULL
