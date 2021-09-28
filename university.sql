a61ce1a8ede8ff3a4463361e820ea3d9d272378a
mirkone1994 <mirkochiappori@gmail.com>
2021-09-28 16:16:27 +0200

    2

--- university.sql
+++ university.sql
@@ -2,3 +2,7 @@
 SELECT COUNT(`name`), YEAR(`enrolment_date`)
 FROM `students`
 GROUP BY YEAR(`enrolment_date`)
+--2 ntare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(`id`), `office_address`
+FROM `teachers`
+GROUP BY `office_address`
