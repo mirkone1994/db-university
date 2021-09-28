05f2a58339bdae6a2201a616ecc73b4afec981a6
mirkone1994 <mirkochiappori@gmail.com>
2021-09-28 16:35:33 +0200

    4 select

--- university.sql
+++ university.sql
@@ -15,3 +15,6 @@ WHERE `cfu` > 10
 --3 (select) Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT * FROM `students`
 WHERE YEAR(`date_of_birth`) > 1991
+--4(select) Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * FROM `courses`  
+WHERE `year`= 1 AND `period` = 'I semestre'
