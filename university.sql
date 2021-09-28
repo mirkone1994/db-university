46ebf71f3c175b42e6b5435504eef2d5c613a0b4
mirkone1994 <mirkochiappori@gmail.com>
2021-09-28 16:20:46 +0200

    1 select

--- university.sql
+++ university.sql
@@ -1,8 +1,11 @@
---1 Contare quanti iscritti ci sono stati ogni anno
+--1(group by) Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(`name`), YEAR(`enrolment_date`)
 FROM `students`
 GROUP BY YEAR(`enrolment_date`)
---2 ntare gli insegnanti che hanno l'ufficio nello stesso edificio
+--2(group by) Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT COUNT(`id`), `office_address`
 FROM `teachers`
 GROUP BY `office_address`
+--1(select)
+SELECT * FROM `students`
+WHERE YEAR(`date_of_birth`) = 1990  
